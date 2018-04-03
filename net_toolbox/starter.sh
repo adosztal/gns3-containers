38a9ef65b5ac3af6304a9f8ff264a2aff764e96a
adosztal <adosztal@gmail.com>
2018-04-03 21:17:07 +0200

    Fixes

--- net_toolbox/starter.sh
+++ net_toolbox/starter.sh
@@ -6,6 +6,7 @@ chmod 777 /tftpboot/
 
 # (Re)starting services
 # snmpd hangs after stop, it needs to be killed manually
+/etc/init.d/rsyslog start >/dev/null 2>/dev/null
 /etc/init.d/nginx start >/dev/null 2>/dev/null
 /etc/init.d/vsftpd start >/dev/null 2>/dev/null
 /etc/init.d/tftpd-hpa start >/dev/null 2>/dev/null
@@ -13,7 +14,7 @@ SNMPD_PID=`cat /run/snmpd.pid`
 /etc/init.d/snmpd stop >/dev/null 2>/dev/null
 kill $SNMPD_PID
 /etc/init.d/snmpd start >/dev/null 2>/dev/null
-pkill -HUP rsyslog
+/etc/init.d/snmptrapd start
 
 clear
 
