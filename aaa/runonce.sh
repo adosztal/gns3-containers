973f342d505184148f4c89817e50f2a88303ad64
adosztal <adosztal@gmail.com>
2018-04-08 20:13:58 +0200

    Added runonce script

---
+++ aaa/runonce.sh
@@ -0,0 +1,10 @@
+touch /var/log/tac_plus.acct
+chmod 644 /var/log/tac_plus.acct
+chmod 644 /etc/freeradius/radiusd.conf
+chmod 644 /etc/freeradius/clients.conf
+chmod 644 /etc/freeradius/eap.conf
+chmod 644 /etc/freeradius/modules/mschap
+chmod 644 /etc/freeradius/users
+chmod 644 /etc/tacacs+/tac_plus.conf
+chmod 755 /sbin/starter.sh
+
