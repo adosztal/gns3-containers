9e92c1e25f0fb18312b6346ce1306d29afa659e6
adosztal <adosztal@gmail.com>
2018-04-08 18:49:27 +0200

    Initial commit: AAA

---
+++ aaa/README.md
@@ -0,0 +1,3 @@
+# AAA server
+
+This appliance provides RADIUS and TACACS+ services with preconfigured users and groups.
---
+++ aaa/starter.sh
@@ -0,0 +1,10 @@
+# Starting services
+/etc/init.d/freeradius start >/dev/null 2>/dev/null
+/etc/init.d/tacacs+ start >/dev/null 2>/dev/null
+
+clear
+
+# Launching shell
+cd
+exec bash -i
+
