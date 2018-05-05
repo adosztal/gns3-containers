75ca7877507c795a49e25a13d5bf59867ce90291
adosztal <adosztal@gmail.com>
2018-05-05 14:18:51 +0200

    Updated to FreeRADIUS 3.0 config format

--- aaa/runonce.sh
+++ aaa/runonce.sh
@@ -1,10 +1,11 @@
 touch /var/log/tac_plus.acct
+chown freerad:adm /var/log/tac_plus.acct
 chmod 644 /var/log/tac_plus.acct
-chmod 644 /etc/freeradius/radiusd.conf
-chmod 644 /etc/freeradius/clients.conf
-chmod 644 /etc/freeradius/eap.conf
-chmod 644 /etc/freeradius/modules/mschap
-chmod 644 /etc/freeradius/users
+chown freerad:freerad /etc/freeradius/3.0/clients.conf
+chmod 644 /etc/freeradius/3.0/clients.conf
+chown freerad:freerad /etc/freeradius/3.0/mods-config/files/authorize
+chmod 644 /etc/freeradius/3.0/mods-config/files/authorize
+chown freerad:freerad /etc/tacacs+/tac_plus.conf
 chmod 644 /etc/tacacs+/tac_plus.conf
 chmod 755 /sbin/starter.sh
 
